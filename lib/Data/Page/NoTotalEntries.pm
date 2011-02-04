3fd67910f32ba3463d1835430243920de847f92a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-02-04 12:42:48 +0900

    initial import

---
+++ lib/Data/Page/NoTotalEntries.pm
@@ -0,0 +1,39 @@
+package Data::Page::NoTotalEntries;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Data::Page::NoTotalEntries -
+
+=head1 SYNOPSIS
+
+  use Data::Page::NoTotalEntries;
+
+=head1 DESCRIPTION
+
+Data::Page::NoTotalEntries is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
